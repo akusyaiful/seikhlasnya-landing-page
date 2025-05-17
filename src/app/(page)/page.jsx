101c8bef163248bd96de6124aa88ff8be786894c
Msyaifullah <msyaifullah13@gmail.com>
2025-05-17 22:02:31 +0700

    init page folder

---
+++ src/app/(page)/page.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { VStack, Text, Mark, Separator } from "@chakra-ui/react";
+import GreetingSection from "./components/GreetingSection";
+import DonationForm from "./components/DonationForm";
+import BottomNavigation from "@/components/client/BottomNavigation";
+
+const Home = () => {
+  return (
+    <VStack position={"relative"} minH={"vh"}>
+      <GreetingSection />
+      <DonationForm />
+      <BottomNavigation />
+    </VStack>
+  );
+};
+
+export default Home;
