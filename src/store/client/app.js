d9fa3d977cf95d5a4cfac390e539d4ee18ed9707
akusyaiful <msyaifullah13@gmail.com>
2025-06-01 15:27:50 +0700

    delete unused file

--- src/store/client/app.js
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-
-export const useAppStore = create((set) => ({
-  openModalUnauthenticated: false,
-  updateOpenModalUnauthenticated: (value) =>
-    set(() => ({ openModalUnauthenticated: value })),
-}));
